7b78775551fe28c0bba99b9d2ddd1f3abf7b662f
Terrionna <tkmcghee@gmail.com>
2017-03-30 18:30:44 -0400

    created the local strategy with handler function to authenticate users with passport js.

---
+++ config/passport.js
@@ -0,0 +1,26 @@
+// this is the configuration of the passport node module
+const passport = require('passport') // this is the module not the file
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('../models/user.model');
+
+passport.use(new LocalStrategy({
+    usernameField: 'email'
+}),
+function howWeAuth(username, password, done){
+  User.findOne({email: username}, function(err, user){
+    if(err){
+      return done(err);
+    }
+    if(!user){ // if there is not a user with that email
+      return done(null, false, {
+        msg: 'User Not Found'
+      });
+    }
+    if(!user.validPassword(password)){
+      return done(null, false, {
+        msg: 'Authentication Failed'
+      })
+    }
+    return done(null, user);
+  });
+});
